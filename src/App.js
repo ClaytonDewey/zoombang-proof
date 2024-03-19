4f393f84d217ef3e307d8d0afd0927e3b1cec507
Clayton Dewey <clay@dryadmedia.com>
2024-03-19 09:01:43 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
+console.log(data);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <h1>ZoomBang Proofs</h1>
       </header>
+      <main>
+        {data.map((item, index) => {
+          return (
+            <details key={index}>
+              <summary key={index}>{item.title}</summary>
+
+              <div className='image-grid'>
+                {item.images.map((image, index) => (
+                  <div className='image-wrapper' key={index}>
+                    <img src={image} alt='' />
+                  </div>
+                ))}
+              </div>
+            </details>
+          );
+        })}
+      </main>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,59 @@
+const data = [
+  {
+    title: 'ZB0020',
+    images: [
+      './images/ZB0020/ZB0020-Back.jpg',
+      './images/ZB0020/ZB0020-Front.jpg',
+      './images/ZB0020/ZB0020-Side.jpg',
+      './images/ZB0020/ZB0020-Inside.jpg',
+      './images/ZB0020/ZB0020Blk-Back.jpg',
+      './images/ZB0020/ZB0020Blk-Front.jpg',
+      './images/ZB0020/ZB0020Blk-Side.jpg',
+      './images/ZB0020/ZB0020Blk-Inside.jpg',
+    ],
+  },
+  {
+    title: 'ZB0016',
+    images: [
+      './images/ZB0016/ZB0016-Front.jpg',
+      './images/ZB0016/ZB0016-Back.jpg',
+      './images/ZB0016/ZB0016-Box-Back.jpg',
+      './images/ZB0016/ZB0016-Box-Front.jpg',
+      './images/ZB0016/ZB0016-Box-Front-alternate.jpg',
+    ],
+  },
+  {
+    title: 'ZB1215',
+    images: [
+      './images/ZB1215/ZB1215-Box-Front.jpg',
+      './images/ZB1215/ZB1215-Box-Back.jpg',
+      './images/ZB1215/ZB1215-Buttstock.jpg',
+      './images/ZB1215/ZB1215-Dbl-Buttstock.jpg',
+      './images/ZB1215/ZB1215-Grouped.jpg',
+      './images/ZB1215/ZB1215.jpg',
+    ],
+  },
+  {
+    title: 'Competitor Vests',
+    images: [
+      './images/ShootingVests/competitor-vest-1.jpg',
+      './images/ShootingVests/competitor-vest-2.jpg',
+      './images/ShootingVests/competitor-vest-3.jpg',
+      './images/ShootingVests/competitor-vest-1-alternate.jpg',
+      './images/ShootingVests/competitor-vest-4.jpg',
+      './images/ShootingVests/competitor-vest-5.jpg',
+      './images/ShootingVests/competitor-vest-2-alternate.jpg',
+    ],
+  },
+  {
+    title: 'Chest Shields',
+    images: [
+      './images/Chest-Shields/ZB5110.jpg',
+      './images/Chest-Shields/ZB5120.1.jpg',
+      './images/Chest-Shields/ZB5122.1.jpg',
+      './images/Chest-Shields/ZB5128C.jpg',
+    ],
+  },
+];
+
+export default data;
